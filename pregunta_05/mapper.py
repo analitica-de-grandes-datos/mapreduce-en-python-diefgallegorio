3ef1346cf0ddf9ef68962af444cdb702928c7272
Diego Gallego Ríos <diefgallegorio@unal.edu.co>
2023-06-13 22:44:14 -0500

    Respuestas 5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,7 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        data = (line.split("   ")[1].split('-')[1])
+        sys.stdout.write("{}\t1\n".format(data))
